5ba52be5a5a70007dda9d177a54354a66921a74d
QuentinTlgn <tauleigq@3il.fr>
2023-11-23 14:29:50 +0100

    Test fizz (multiple de 3) OK

--- tptests/fizzbuzz.java
+++ tptests/fizzbuzz.java
@@ -2,6 +2,9 @@ package tptests;
 
 public class fizzbuzz {
 	public static String fizzBuzz(int i) {
+		if(i%3 == 0 && i!=0) {
+			return "Fizz";
+		}
 		return Integer.toString(i);
 	}
 	
--- tptests/fizzbuzztest.java
+++ tptests/fizzbuzztest.java
@@ -20,5 +20,15 @@ public class fizzbuzztest {
 	public void test2() {
 		assertEquals(fizzbuzz.fizzBuzz(2), "2");
 	}
+	
+	@Test
+	public void test3() {
+		assertEquals(fizzbuzz.fizzBuzz(3), "Fizz");
+	}
+	
+	@Test
+	public void test6() {
+		assertEquals(fizzbuzz.fizzBuzz(6), "Fizz");
+	}
 
 }
