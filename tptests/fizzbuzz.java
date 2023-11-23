bf20f7fd127a17995bd6bcdec60e022e9582b924
QuentinTlgn <tauleigq@3il.fr>
2023-11-23 14:18:59 +0100

    test 2

--- tptests/fizzbuzz.java
+++ tptests/fizzbuzz.java
@@ -2,6 +2,8 @@ package tptests;
 
 public class fizzbuzz {
 	public static String fizzBuzz(int i) {
+		if(i == 1)
+			return "1";
 		return "0";
 	}
 	
--- tptests/fizzbuzztest.java
+++ tptests/fizzbuzztest.java
@@ -7,8 +7,13 @@ import org.junit.Test;
 public class fizzbuzztest {
 
 	@Test
-	public void test() {
+	public void test0() {
 		assertEquals(fizzbuzz.fizzBuzz(0), "0");
+	}
+	
+	@Test
+	public void test1() {
+		assertEquals(fizzbuzz.fizzBuzz(1), "1");
 	}
 
 }
