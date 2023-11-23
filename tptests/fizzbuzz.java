8b85ade3ffa087ed3ebc9fc841522093a1ff7203
QuentinTlgn <tauleigq@3il.fr>
2023-11-23 14:16:26 +0100

    test 1

---
+++ tptests/fizzbuzz.java
@@ -0,0 +1,8 @@
+package tptests;
+
+public class fizzbuzz {
+	public static String fizzBuzz(int i) {
+		return "0";
+	}
+	
+}
---
+++ tptests/fizzbuzztest.java
@@ -0,0 +1,14 @@
+package tptests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class fizzbuzztest {
+
+	@Test
+	public void test() {
+		assertEquals(fizzbuzz.fizzBuzz(0), "0");
+	}
+
+}
